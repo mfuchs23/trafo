b8c6e542d44e4650be8fc73fdd2dc140f93ebd28
Michael Fuchs <michael.fuchs@dbdoclet.org>
2013-07-29 10:03:59 +0200

    Sicherung

---
+++ org.dbdoclet.trafo.html.dita/src/main/java/org/dbdoclet/trafo/internal/html/dita/editor/AbstractBlockEditor.java
@@ -0,0 +1,32 @@
+package org.dbdoclet.trafo.internal.html.dita.editor;
+
+import org.dbdoclet.tag.dita.Body;
+import org.dbdoclet.tag.dita.DitaTagFactory;
+import org.dbdoclet.tag.dita.Topic;
+import org.dbdoclet.trafo.html.EditorException;
+import org.dbdoclet.trafo.html.EditorInstruction;
+import org.dbdoclet.xiphias.dom.NodeImpl;
+
+
+public abstract class AbstractBlockEditor extends DitaEditor {
+
+	@Override
+	public EditorInstruction edit(EditorInstruction values)
+			throws EditorException {
+
+		setValues(super.edit(values));
+		
+		DitaTagFactory tagFactory = getTagFactory();
+		NodeImpl parent = values.getParent();
+		
+		if (parent instanceof Topic) {
+		
+			Body body = tagFactory.createBody();
+			parent.appendChild(body);
+			setParent(body);
+			setCurrent(body);
+		}
+		
+		return finalizeValues();
+	}
+}
